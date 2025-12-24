3a36bd986bd6409010fd210165e24c6a46536f3c
zbrianhuang <67428806+zbrianhuang@users.noreply.github.com>
2025-12-24 11:12:52 -0500

    update

--- dft.py
+++ dft.py
@@ -1,16 +1,10 @@
 import numpy as np
 
 def DFT(y):
-    """
-    Compute the Discrete Fourier Transform of a 1D array y.
-    """
     N = len(y)
-    # Create an array to store the DFT results (complex numbers)
     dft_result = np.zeros(N, dtype=np.complex128)
 
-    # Iterate over each frequency bin k
     for k in range(N):
-        # Calculate the DFT for frequency k
         summation = 0
         for n in range(N):
             w = complex(0,1) * -2 * np.pi * k * n / N
@@ -21,12 +15,9 @@ def DFT(y):
 
 def IDFT(Y):
     N = len(Y)
-    # Create an array to store the IDFT results (real numbers)
     idft_result = np.zeros(N, dtype=np.float64)
 
-    # Iterate over each time index n
     for n in range(N):
-        # Calculate the IDFT for time n
         summation = 0
         for k in range(N):
             w = complex(0,1) * 2 * np.pi * k * n / N
